ba8186acc8d3d3077243a118423af93d831cda5c
YishakG <111497293+YishakG@users.noreply.github.com>
2025-03-04 18:57:57 +0300

    Time: 6 ms (39.28%), Space: 21.9 MB (79.54%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.py
+++ 0890-lemonade-change/0890-lemonade-change.py
@@ -1,23 +1,44 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        moneys = {5:0,10:0,20:0}
+        five_count = ten_count = 0
         for bill in bills:
-            moneys[bill] += 1
-            change = bill - 5
-            if change == 5:
-                if moneys[5] < 1:
+            if bill == 5:
+                five_count += 1
+            elif bill == 10:
+                if five_count == 0:
                     return False
-                else:
-                    moneys[5] -= 1
-            elif change == 15:
-                if moneys[5] >= 1 and moneys[10] >= 1:
-                    moneys[5] -= 1
-                    moneys[10] -= 1
-                elif moneys[5] >= 3:
-                    moneys[5] -= 3
+                five_count -= 1
+                ten_count += 1
+            elif bill == 20:
+                if ten_count > 0 and five_count > 0:
+                    ten_count -= 1
+                    five_count -= 1
+                elif five_count >= 3:
+                    five_count -= 3
                 else:
                     return False
         return True
+        # no need of dictionary
+        # no need of keeping track of the 20
+        # 
+        # moneys = {5:0,10:0,20:0}
+        # for bill in bills:
+        #     moneys[bill] += 1
+        #     change = bill - 5
+        #     if change == 5:
+        #         if moneys[5] < 1:
+        #             return False
+        #         else:
+        #             moneys[5] -= 1
+        #     elif change == 15:
+        #         if moneys[5] >= 1 and moneys[10] >= 1:
+        #             moneys[5] -= 1
+        #             moneys[10] -= 1
+        #         elif moneys[5] >= 3:
+        #             moneys[5] -= 3
+        #         else:
+        #             return False
+        # return True
 
 
 
