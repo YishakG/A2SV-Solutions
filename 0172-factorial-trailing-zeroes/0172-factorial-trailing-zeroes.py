844a77ae3fa2eede81c917b300b0111cc4b26319
YishakG <111497293+YishakG@users.noreply.github.com>
2025-03-11 18:37:51 +0300

    Time: 0 ms (100%), Space: 17.9 MB (18.3%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -1,15 +1,21 @@
 class Solution:
+    # # o(n) approach
+    # def factorial(self,n):
+    #     if n <= 1:
+    #         return 1
+    #     return n * self.factorial(n - 1)
+    # def trailingZeroes(self, n: int) -> int:    
+    #     fact = self.factorial(n)
+    #     trailing_zeros = 0
+    #     while fact % 10 == 0:
+    #         trailing_zeros += 1
+    #         fact //= 10
+    #     return trailing_zeros
     def factorial(self,n):
-        if n <= 1:
-            return 1
-        
-        return n * self.factorial(n - 1)
-    def trailingZeroes(self, n: int) -> int:    
-        fact = self.factorial(n)
-        trailing_zeros = 0
-        while fact % 10 == 0:
-            trailing_zeros += 1
-            fact //= 10
-        return trailing_zeros
+        if n < 5:
+            return 0
+        return n // 5 + self.factorial(n//5)
+    def trailingZeroes(self, n: int) -> int: 
+        return self.factorial(n)
          
          
