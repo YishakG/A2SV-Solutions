5c4f65e63e6f39f687b43e3b3b3aa34bdd514d1b
YishakG <111497293+YishakG@users.noreply.github.com>
2025-03-21 18:49:26 +0300

    Time: 4 ms (3.86%), Space: 18.1 MB (12.12%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,14 +1,17 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        if rowIndex == 0:
-            return [1]
-        prev_row = self.getRow(rowIndex-1)
-        
-        row = [1]
-        for i in range(1,len(prev_row)):
-            row.append(prev_row[i-1]+prev_row[i])
-        row.append(1)
+        memo = defaultdict(int)
+        def get(r,c):
+            if (r,c) in memo:
+                return memo[(r,c)]
+            if c == 0 or r == c:
+                return 1
 
+            memo[(r,c)] = get(r-1,c-1) + get(r-1,c)
+            return memo[(r,c)]
 
-        return row
-        
+        ans = [0] * (rowIndex + 1)
+        for col in range(rowIndex + 1):
+            ans[col] += get(rowIndex,col)
+        return ans
+         
