66f41b088309c4a307ce011b8056a641e28e591c
YishakG <111497293+YishakG@users.noreply.github.com>
2025-03-21 18:29:34 +0300

    Time: 0 ms (100%), Space: 17.8 MB (50.2%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,25 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        """
+        n + -
+        n odd even
+        
+        """
+        if n == 0:
+            return 1
+
+        def pwer(x,n):
+            if n == 1:
+              return x
+            a = b = self.myPow(x,n//2)
+            if n % 2:
+                b *= x
+            return a * b
+        
+        ans = pwer(x,abs(n))
+        if n < 0:
+            return 1 / ans
+        
+        return ans
+
+        
