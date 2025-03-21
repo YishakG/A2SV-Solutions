07ddc3906ae260736264124ce4fcaa3d952aa036
YishakG <111497293+YishakG@users.noreply.github.com>
2025-03-21 18:50:35 +0300

    Time: 0 ms (100%), Space: 18.1 MB (12.12%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,14 +1,12 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        memo = defaultdict(int)
+
+        @cache
         def get(r,c):
-            if (r,c) in memo:
-                return memo[(r,c)]
             if c == 0 or r == c:
                 return 1
 
-            memo[(r,c)] = get(r-1,c-1) + get(r-1,c)
-            return memo[(r,c)]
+            return get(r-1,c-1) + get(r-1,c)
 
         ans = [0] * (rowIndex + 1)
         for col in range(rowIndex + 1):
