4211d824c01c36fa2b573b8a5458b646c5767e0e
YishakG <111497293+YishakG@users.noreply.github.com>
2025-06-24 15:27:23 +0300

    Time: 7 ms (37.96%), Space: 18.7 MB (72.42%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,21 @@
 from collections import defaultdict
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        count = defaultdict(int)
-        for num in nums:
-            count[num] += 1
-        temp = len(nums) + 1
-        for i in range(temp):
-            if count[i] == 0:
-                return i
+        # x ^ 0 = x and x ^ x = 0
+                
+        ans = len(nums)
+        for i in range(ans):
+            ans ^= i ^ nums[i]
+        return ans
+
+
+        # O(n) time complexity O(n) space complexity
+        # count = defaultdict(int)
+        # for num in nums:
+        #     count[num] += 1
+        # temp = len(nums) + 1
+        # for i in range(temp):
+        #     if count[i] == 0:
+        #         return i
+        # Sum Method
+        # return expected_sum(sum from 0 to n) - actual_sum(sum(nums))
