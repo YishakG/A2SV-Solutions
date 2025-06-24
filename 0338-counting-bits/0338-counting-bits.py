9af2d7f41490a7e42fea848b18e79dd2d3717dbc
YishakG <111497293+YishakG@users.noreply.github.com>
2025-06-24 14:35:37 +0300

    Time: 6 ms (54.12%), Space: 18.3 MB (98.4%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,9 +1,16 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = []
-        for i in range(0,n+1):
-            temp = str(bin(i))
-            ans.append(temp.count("1"))
-        return ans
+        ans = [0] * (n + 1)
+        for i in range(1,n+1):
+            # The number of 1 in i equals number of 1's in i//2 plus the last bit
+            # O(n) Time Complexity O(1) space complexity
+            ans[i] = ans[ i >> 1] + (i & 1)
+        return ans 
+        #Not efficient for large numbers
+        # ans = []
+        # for i in range(0,n+1):
+        #     temp = str(bin(i))
+        #     ans.append(temp.count("1"))
+        # return ans
 
         
