51d1459705bf72bcb3367e669b14e44c1208100f
YishakG <111497293+YishakG@users.noreply.github.com>
2025-07-27 10:30:30 +0300

    Time: 16 ms (1.96%), Space: 17.7 MB (88.3%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,10 +1,9 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
+        # Time Complexity: O(log(n)) Space Complexity: O(log(n)) base 4 but doesnt matter
         if n == 1:
             return True
         if 0 <= n < 1:
             return False
         return self.isPowerOfFour(n/4)
         
-        
-         
