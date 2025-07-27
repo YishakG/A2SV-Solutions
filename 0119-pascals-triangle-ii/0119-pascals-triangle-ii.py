94496a8060966591605cf6d10e7f03b4f7aab618
YishakG <111497293+YishakG@users.noreply.github.com>
2025-07-27 12:11:34 +0300

    Time: 3 ms (10.15%), Space: 17.7 MB (81.51%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,15 +1,15 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
+        # Time Complexity: O(k^2) Space Complexity: O(k) k = rowIndex
+        if rowIndex == 0:
+            return [1]
+        prev_row = self.getRow(rowIndex-1)
+        
+        row = [1]
+        for i in range(1,len(prev_row)):
+            row.append(prev_row[i-1]+prev_row[i])
+        row.append(1)
 
-        @cache
-        def get(r,c):
-            if c == 0 or r == c:
-                return 1
 
-            return get(r-1,c-1) + get(r-1,c)
-
-        ans = [0] * (rowIndex + 1)
-        for col in range(rowIndex + 1):
-            ans[col] += get(rowIndex,col)
-        return ans
-         
+        return row
+        
