1d24a316b7a76b2dd8a695d93468cc2a0441d659
YishakG <111497293+YishakG@users.noreply.github.com>
2025-07-28 21:59:58 +0300

    Time: 0 ms (100%), Space: 17.6 MB (80.73%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -1,21 +1,6 @@
 class Solution:
-    # # o(n) approach
-    # recursive method of factorial but iterative for trailing 
-    # def factorial(self,n):
-    #     if n <= 1:
-    #         return 1
-    #     return n * self.factorial(n - 1)
-    # def trailingZeroes(self, n: int) -> int:    
-    #     fact = self.factorial(n)
-    #     trailing_zeros = 0
-    #     while fact % 10 == 0:
-    #         trailing_zeros += 1
-    #         fact //= 10
-    #     return trailing_zeros
-    
-    def trailingZeroes(self, n: int) -> int: 
+    def trailingZeroes(self, n: int) -> int:
+        # Time Complexity: O(log(n)) base 5 Space Complexity: O(log(n)) base 5
         if n < 5:
             return 0
-        return n // 5 + self.trailingZeroes(n//5)
-         
-         
+        return n//5 + self.trailingZeroes(n//5)
